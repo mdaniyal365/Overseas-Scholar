bb70d3034a19c160b64f78a4d1bb6262e081e69c
daniyal <mohddaniyal365@gmail.com>
2024-02-08 23:31:19 +0530

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import NavBar from "./components/NavBar"
+import IeltsPreparation from "./pages/ieltsPreparation"
 export default function App() {
   return (<>
   <NavBar/>
+  {/* <IeltsPreparation/> */}
   </>
     
   )
---
+++ src/pages/IeltsPreparation.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+function IeltsPreparation() {
+  return (
+    <div>
+      
+    <section>
+      <div class="md:px-32 px-4  pb-28" style="background:linear-gradient(180deg, #F0F3FF 0%, #FFF2F3 100%)">
+        <div class="grid md:grid-cols-2">
+          <div class="flex flex-col justify-center gap-y-6 text-center md:text-left">
+            <h1 class="font-bold text-[32px] md:text-[42px] leading-tight my-2">Crack IELTS with 7+ Bands in 4 Weeks</h1>
+            <div class="text-[16px] md:text-[20px] text-[#3A3A3A] leading-8">
+              <ul>
+                <li class="pb-4">Over 2,50,000+ IELTS aspirants got their <h2 class="inline-block">dream IELTS Band with LeapScholar</h2></li>
+                </ul>
+                </div>
+                </div>
+                <div class="md:row-span-2 relative mt-6 md:my-6"><img alt='lofda'/>
+                </div><div class="my-4">
+                  <div class="flex items-center justify-center md:block">
+                    <button class="bg-yellow-to-orange cursor-pointer text-white px-12 py-2 font-bold text-[20px] rounded md:p-4 pb">Try Free MasterClass</button>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    <div class="bottom-[80px] md:bottom-[100px] rounded-10 max-w-[320px] md:max-w-[1120px] bg-white m-auto relative z-2 shadow-1">
+                      <div class="flex flex-col py-10 gap-4 rounded-2xl">
+                        <h2 class="font-bold text-lg md:text-[32px] text-center md:px-4">Why choose Leap Scholar?</h2>
+                        <div class="grid grid-cols-2 md:grid-cols-4">
+                          <div class="py-4 md:py-8">
+                            <div class="flex flex-col items-center justify-center border-[#E8E8E8] px-4 ">
+                              <img alt="Learn from India's top IELTS Trainers" loading="eager" width="70" height="70" decoding="async" data-nimg="1" class="md:w-24 pb-4" style="color:transparent" src="https://d14lg9nzq1d3lc.cloudfront.net/avataran/avataran_assets/images/ielts/featuresSection/students_trained_icon.svg"/>
+                              <div class="text-center font-bold text-[#0A0926] md:text-[18px] md:px-4">Learn from India's top IELTS Trainers</div>
+                              </div>
+                              </div>
+                              <div class="py-4 md:py-8"><div class="flex flex-col items-center justify-center border-[#E8E8E8] px-4 ">
+                                <img alt="90% score 7+ bands" loading="eager" width="70" height="70" decoding="async" data-nimg="1" class="md:w-24 pb-4" style="color:transparent" src="https://d14lg9nzq1d3lc.cloudfront.net/avataran/avataran_assets/images/ielts/featuresSection/bands_Icon.svg"/>
+                                <div class="text-center font-bold text-[#0A0926] md:text-[18px] md:px-4">90% score 7+ bands</div>
+                                </div>
+                                </div>
+                                <div class="py-4 md:py-8"><div class="flex flex-col items-center justify-center border-[#E8E8E8] px-4 ">
+                                  <img alt="Average student rating of 4.5/5" loading="eager" width="70" height="70" decoding="async" data-nimg="1" class="md:w-24 pb-4" style="color:transparent" src="https://d14lg9nzq1d3lc.cloudfront.net/avataran/avataran_assets/images/ielts/featuresSection/average_student_rating_icon.svg"/>
+                                  <div class="text-center font-bold text-[#0A0926] md:text-[18px] md:px-4">Average student rating of 4.5/5</div>
+                                  </div>
+                                  </div>
+                                  <div class="py-4 md:py-8"><div class="flex flex-col items-center justify-center border-[#E8E8E8] px-4 ">
+                                    <img alt="PAN India community" loading="eager" width="70" height="70" decoding="async" data-nimg="1" class="md:w-24 pb-4" style="color:transparent" src="https://d14lg9nzq1d3lc.cloudfront.net/avataran/avataran_assets/images/ielts/featuresSection/pan_india_community.svg"/>
+                                    <div class="text-center font-bold text-[#0A0926] md:text-[18px] md:px-4">PAN India community</div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </section>
+
+      
+    </div>
+  )
+}
+
+export default IeltsPreparation
